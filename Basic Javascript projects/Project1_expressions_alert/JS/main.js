ef8bb0a5d090b6a637e068f3dfb2ab8bff702054
piet-de-leeuw <pietdeleeuw156@gmail.com>
2024-05-24 16:35:37 +0200

    Removed an unnecessary folder and added Project1_expressions_alert

--- Basic Javascript projects/Project1_expressions_alert/JS/main.js
+++
@@ -1,9 +0,0 @@
-window.alert("This is an basic exercise for JavaScript."); //Prints this text in a alertbox.
-
-var sent1 = "This is the beginning of the string,"; //declare an variable
-var sent2 = " and this is the end of the string.";  //declare an variable
-
-var sentence = sent1 + sent2;   //Concatinate sent1 and sent2
-
-document.write(sentence); // Prints the valeu of sentence to the screen.
-
---
+++ Project1_expressions_alert/JS/main.js
@@ -0,0 +1,10 @@
+window.alert("This is an basic exercise for JavaScript."); // Prints this text in a alertbox.
+
+var sent1 = "This is the beginning of the string,"; // Declare an variable
+var sent2 = " and this is the end of the string.";  // Declare an variable
+
+var sentence = sent1 + sent2;   // Concatinate sent1 and sent2
+document.write(sentence); // Prints the valeu of sentence to the screen.
+
+var sum = 503 + 2234; // Declears an exspresion in a variable.
+document.write(sum);  // Prints the value of sum to the screen.
